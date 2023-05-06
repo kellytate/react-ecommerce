5bda67fb9f8d71d9e7cfc06dcccf03b23411ec73
kelly <kelly.tate.dev@gmail.com>
2023-05-06 16:35:17 -0700

    Adds cart to local storage.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cart from "../Components/Cart";
 import NavBar from "../Components/NavBar";
 import ProductTable from "../Components/ProductTable";
@@ -6,6 +6,12 @@ import ProductTable from "../Components/ProductTable";
 function Home() {
   const [open, setOpen] = useState(false);
   const [cart, updateCart] = useState([]);
+  useEffect(() => {
+    window.localStorage.setItem('CURRENT_CART_STATE', JSON.stringify(cart));
+    console.log(cart);
+  }, [cart]);
+
+  
 
   return (
     <main>
