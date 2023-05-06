100818691d455883ce30c708ec640abb10ab64d5
kelly <kelly.tate.dev@gmail.com>
2023-05-06 16:40:39 -0700

    Persists Cart after refresh

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,12 +6,15 @@ import ProductTable from "../Components/ProductTable";
 function Home() {
   const [open, setOpen] = useState(false);
   const [cart, updateCart] = useState([]);
+
+  useEffect(() => {
+    const data = window.localStorage.getItem('CURRENT_CART_STATE');
+    if ( data !== null ) updateCart(JSON.parse(data));
+  }, []);
+
   useEffect(() => {
     window.localStorage.setItem('CURRENT_CART_STATE', JSON.stringify(cart));
-    console.log(cart);
   }, [cart]);
-
-  
 
   return (
     <main>
